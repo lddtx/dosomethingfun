44e0d3bfcb1331322e44d91dfe7c6a82309d16dc
lddtx <1445101933@qq.com>
2020-11-09 23:05:43 +0800

    【2020-11-09】测试lang3

--- src/main/java/com/dosomethingfun/first/App.java
+++ src/main/java/com/dosomethingfun/first/App.java
@@ -1,7 +1,13 @@
 package com.dosomethingfun.first;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class App {
     public static void main(String[] args) {
+        String str = "i";
+        if (StringUtils.isNotEmpty(str)) {
+            System.out.println(str);
+        }
         System.out.println("begin");
     }
 }
